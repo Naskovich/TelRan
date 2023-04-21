185174e5b4eebea820c70fda2d601a6d9ea35194
Naskovich <129493745+Naskovich@users.noreply.github.com>
2023-04-22 00:17:27 +0200

    Hw FE №5 21.04.2023

---
+++ Hw FE №5 (21.04.2023)/script.js
@@ -0,0 +1,55 @@
+//1.Написать программу, которая через prompt 
+//считывает число и выводит в консоль 
+//число равное 10% от введенного числа
+
+// const number = +prompt("number")
+// const proizv = number * 0.1
+// console.log(proizv)
+
+//2.Написать программу, которая получает два числа 
+//и выводит наименьшее
+
+//const number1 = +prompt("First number?")
+//const number2 = +prompt("Second number?")
+// if (number1 < number2) {
+//     console.log("number1")
+// }
+// else if (number1 > number2) {
+//     console.log("number2")
+// }
+
+//3.Написать программу, которая считывает через prompt число
+//и выводит одно из следующих сообщений:
+ //‘Число меньше 100’, ‘Число больше 100’ 
+ //или ‘Число равно 100’
+
+//  const number = +prompt("number")
+
+//  if (number < 100) {
+//      console.log("number<100")
+//  }
+//  else if (number > 100) {
+//      console.log("number>100")
+//  }
+//  else {
+//      console.log("number == 100")
+//  }
+
+//4. Написать программу, которая запрашивает у пользователя
+//  его имя и возраст(в годах)
+//  и выводит в консоль сообщение ‘Hello, <name>’,
+//     если пользователь совершеннолетний,
+//     или ‘Hi, <name>’, если пользователь несовершеннолетний.
+
+// const userName1 = prompt("What is your name?")
+// const userName2 = prompt("How old are you?")
+// if (userName2 > 18) {
+//     console.log("Hello, <name>")
+// }
+// else if (userName2 < 18) {
+//     console.log("Hi, <name>",)
+// }
+// else if (userName2 == 18) {
+//     console.log("Hello, <name>",)
+// }
+
