cae4f7821e96872d468cfcae55744c8c8b396d84
Naskovich <129493745+Naskovich@users.noreply.github.com>
2023-05-23 10:05:23 +0200

    Add files via upload
    HomeWork 1-2

---
+++ ScannerHW/HomeWork.java
@@ -0,0 +1,71 @@
+package ScannerHW;
+
+public class HomeWork {
+
+    public static void main(String[] args) {
+
+        int q = 126;
+
+        System.out.println(q / 2);    // 63
+        System.out.println(q % 2);             // 0
+
+        System.out.println(63 / 2);  // 31
+        System.out.println(63 % 2);            // 1
+
+        System.out.println(31 / 2);  // 15
+        System.out.println(31 % 2);            // 1
+
+        System.out.println(15 / 2);  // 7
+        System.out.println(15 % 2);             // 1
+
+        System.out.println(7 / 2);  // 3
+        System.out.println(7 % 2);             // 1
+
+        System.out.println(3 / 2);  // 1
+        System.out.println(3 % 2);             // 1
+
+        System.out.println(1 / 2);  // 0
+        System.out.println(1 % 2);             // 1
+
+        int q2 = 0b1111110;
+        System.out.println(q2);
+
+        //обратный перевод из двоичной системы в десятичную
+        System.out.println(Integer.parseInt("1111110",2));
+    }
+}
+
+       /* System.out.println(q / 8);  // 15
+        System.out.println(q % 8);             // 6
+
+        System.out.println(15 / 8); // 1
+        System.out.println(15 % 8);            // 7
+
+        System.out.println(1 / 8);  // 0
+        System.out.println(1 % 8);            // 1
+
+        int q8 = 0176;
+        System.out.println(q8);
+        }
+        }*/
+
+
+       /* System.out.println(q / 16);  // 7
+        System.out.println(q % 16);             // 14 > e
+
+        System.out.println(7 / 16); // 0
+        System.out.println(7 % 16);            // 7
+
+        int q16 = 0x7e;
+        System.out.println(q16);
+
+    }
+}*/
+
+
+
+
+
+
+
+
