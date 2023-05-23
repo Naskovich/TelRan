6c1934b731e0d9084151cbb5364a22107cd98e1b
Naskovich <129493745+Naskovich@users.noreply.github.com>
2023-05-23 09:45:40 +0200

    Add files via upload
    HW Java ValueBox

---
+++ ValueBox/Box.java
@@ -0,0 +1,8 @@
+package ValueBox;
+
+public class Box {
+    int width = 10;
+    int height = 10;
+    int lenght = 10;
+
+    }
---
+++ ValueBox/Fourth.java
@@ -0,0 +1,10 @@
+package ValueBox;
+
+public class Fourth {
+    public static void main(String[] args) {
+
+        Box box1 = new Box();
+        int valueOfBox = (box1.width * box1.height * box1.lenght);
+        System.out.println( valueOfBox );
+    }
+}
