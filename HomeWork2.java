22c21c1389c371ad405a9e333622201ee24c77a7
Naskovich <129493745+Naskovich@users.noreply.github.com>
2023-06-05 08:50:51 +0200

    Add files via upload
    HomeWork2.java

---
+++ HomeWork2.java
@@ -0,0 +1,17 @@
+package HomeWork;
+
+public class HomeWork2 {
+    public static void main(String[] args) {
+
+        char t = '4';
+        String preResult = String.valueOf(t);
+        int result = Integer.parseInt(preResult);
+        System.out.println(result + 6);
+
+
+        char y = '4';
+        double result1 = Character.getNumericValue(y);
+        System.out.println(result1 + 6);
+
+    }
+}
