af24b6021dffa4c280953649109aa6408f13527c
Naskovich <129493745+Naskovich@users.noreply.github.com>
2023-06-05 18:00:54 +0200

    Add files via upload
    HWCycle1.java

---
+++ HWCycle1.java
@@ -0,0 +1,9 @@
+package HomeWork;
+
+public class HWCycle1 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i < 513; i *= 2)
+            System.out.print(i + " ");
+    }
+}
