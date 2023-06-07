773a65c774d135794bb5d4c00c51d9a8126bc8bd
Naskovich <129493745+Naskovich@users.noreply.github.com>
2023-06-07 22:42:47 +0200

    Add files via upload
    CalculateLift.java

---
+++ CalculateLift.java
@@ -0,0 +1,18 @@
+package HomeWork;
+public class CalculateLift {
+    public static void main(String[] args) {
+        // 1. 1+(50-1)=50
+        // 2. 50+(50-1)=99
+        // 3. 99+(50-1)=148
+        // 4. 148+(50-1)=197(нужен ещё один подъём)
+        // 5. 197+(50-1)=246 (УРА!!!)
+
+        double N = 50;
+        double M = 1;
+        System.out.println(calculate(N, M));
+
+    }
+    public static double calculate(double N, double M) {
+        return (M + 5 * (N - M)) / (N - M);
+    }
+}
