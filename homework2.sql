788b0ddc4787d4638585436181264a1265ec8d82
Naskovich <129493745+Naskovich@users.noreply.github.com>
2023-06-29 19:11:57 +0200

    Add files via upload
    homework2.sql

---
+++ homework2.sql
@@ -0,0 +1,44 @@
+
+
+Задача 1.Вывести телефоны всех поставщиков,
+кроме тех, что из Germany и France.
+
+SELECT SupplierName,
+Phone
+FROM Suppliers
+WHERE
+NOT Country IN ('Germany','France')
+
+
+Задача 2.Добавить два произвольных товара.
+
+INSERT INTO Products(ProductName,SupplierID,CategoryID,Unit,Price)
+VALUES
+('Chocolate', '', '', '', ''),
+('Coca-cola', '', '', '', '');
+
+
+Задача 3.Удалить поставщиков из USA и Germany.
+
+DELETE
+FROM Suppliers
+WHERE Country IN ('USA', 'Germany');
+
+
+Задача 4.Вывести все товары до 20 EUR
+из категорий 3 и 6 по убыванию цены.
+
+SELECT *
+FROM Products
+WHERE
+CategoryID IN (3,6)
+ORDER BY Price DESC
+
+
+Задача 5.Вывести номера трех последних заказов.
+
+ SELECT *
+ FROM Orders
+ ORDER BY OrderID DESC LIMIT 3
+
+
